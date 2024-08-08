23797d1a5054356f9077d483eaa8a9bce5b71335
Lamsy <olamide.adetula.1744@gmail.com>
2024-08-08 19:21:04 +0100

    escrow contract

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -2,26 +2,24 @@ use anchor_lang::prelude::*;
 
 declare_id!("BPSkKKbLFseoabHF5cwdih4V2PUbmnc44j488XSLGDm5");
 
+mod contexts;
+use contexts::*;
+mod state;
+use state::*;
 #[program]
 pub mod escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-    pub fn make(ctx: Context<Initialize>, seed: u64, amount: u64, receive: u64) -> Result<()> {
-        ctx.accounts.save_escrow(seed, receive, ctx.bumps)?;
+    pub fn make(ctx: Context<Make>, seed: u64, amount: u64, receive: u64) -> Result<()> {
+        ctx.accounts.save_escrow(seed, receive, ctx.bumps.escrow)?;
         ctx.accounts.deposit_to_vault(amount)
-        Ok(())
     }
-    pub fn take(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.transfer_to_maker()?;
+        ctx.accounts.withdraw_and_close()
     }
-    pub fn refund(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn refund(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.withdraw_and_close()
     }
 }
 
--- escrow/tests/escrow.ts
+++ escrow/tests/escrow.ts
@@ -1,16 +1,113 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Escrow } from "../target/types/escrow";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import {
+  MINT_SIZE,
+  TOKEN_2022_PROGRAM_ID,
+  createAssociatedTokenAccountIdempotentInstruction,
+  createInitializeMint2Instruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  getMinimumBalanceForRentExemptMint,
+} from "@solana/spl-token";
+import { randomBytes } from "crypto";
 
 describe("escrow", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Escrow as Program<Escrow>;
+  const connection = program.provider.connection;
+  const rpcEndpoint = connection.rpcEndpoint;
+
+  const confirm = async (signature: string): Promise<string> => {
+    const latestBlockHash = await connection.getLatestBlockhash();
+
+    await connection.confirmTransaction({
+      signature,
+      ...latestBlockHash,
+    });
+
+    return signature;
+  };
+  const msg = async (signature: string): Promise<string> => {
+    console.log(
+      `\n ✅ Transaction signature: https://explorer.solana.com/tx/${signature}?cluster=${rpcEndpoint}`
+    );
+    return signature;
+  };
+  const tokenProgram = TOKEN_2022_PROGRAM_ID;
+
+  const seed = new anchor.BN(randomBytes(8));
+
+  const [maker, mintA, taker, mintB] = Array.from({ length: 4 }, () =>
+    Keypair.generate()
+  );
+
+  const [makerAtaA, takerAtaA, makerAtaB, takerAtaB] = [maker, taker].map((k) =>
+    [mintA, mintB]
+      .map((m) =>
+        getAssociatedTokenAddressSync(
+          m.publicKey,
+          k.publicKey,
+          false,
+          tokenProgram
+        )
+      )
+      .flat()
+  );
+
+  //   [
+  //     Buffer.from("escrow"),
+  //     maker.publicKey.toBuffer(),
+  //     seed.toArrayLike(Buffer, "le", 8),
+  //   ],
+  //   program.programId
+  // )[0];
+
+  const escrow = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("escrow"),
+      maker.publicKey.toBuffer(),
+      mintA.publicKey.toBuffer(),
+    ],
+    program.programId
+  )[0];
+
+  const vault = getAssociatedTokenAddressSync(
+    mintA.publicKey,
+    maker.publicKey,
+    false,
+    tokenProgram
+  );
+
+  const accounts = {
+    maker: maker.publicKey,
+    taker: taker.publicKey,
+    makerAtaA,
+    takerAtaA,
+    makerAtaB,
+    takerAtaB,
+    escrow,
+    mintA,
+    mintB,
+    vault,
+    tokenProgram,
+    systemProgram: SystemProgram.programId,
+    rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+  };
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.make().rpc();
     console.log("Your transaction signature", tx);
   });
 });
